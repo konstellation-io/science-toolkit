ae5ebd44a602d77ea679f778a08486948f2a0fd8
Sergio Fernandez <sergio.fernandez@intelygenz.com>
2019-12-04 13:07:39 +0100

    fix(autologin): Add material vue design

--- autologin/public/app.js
+++ autologin/public/app.js
@@ -1,3 +1,5 @@
+
+Vue.use(VueMaterial.default)
 const app = new Vue({
   el: '#app',
   data: {
@@ -7,10 +9,22 @@ const app = new Vue({
       password: ""
     },
     services: {
-      code: { loggedIn: false },
-      minio: { loggedIn: false },
-      gitea: { loggedIn: false },
-      jupyter: { loggedIn: false }
+      code: {
+        loggedIn: false,
+        img: "https://cdn.dribbble.com/users/698/screenshots/3168520/vs-code-icon.png"
+      },
+      minio: {
+        loggedIn: false,
+        img: "https://img.stackshare.io/service/4485/gTawkyAA.png"
+      },
+      gitea: {
+        loggedIn: false,
+        img: "https://pbs.twimg.com/profile_images/892636838397042692/hOlfLnly_400x400.jpg"
+      },
+      jupyter: {
+        loggedIn: false,
+        img: "https://jrogel.com/wp-content/uploads/2015/08/Jupyter.jpg"
+      }
     }
   },
   created() {
@@ -22,10 +36,10 @@ const app = new Vue({
 
   methods: {
 
-    login: async function(event) {
+    login: async function (event) {
       const response = await fetch('/login', {
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         },
         method: 'POST',
         body: JSON.stringify(this.input)
@@ -39,7 +53,7 @@ const app = new Vue({
       })
     },
 
-    logout: function() {
+    logout: function () {
       Object.keys(this.services).forEach(component => {
         const target = this.services[component]
         document.querySelector(`#${component}`).contentWindow.postMessage({ type: 'logout' }, target.url.origin)
@@ -47,7 +61,7 @@ const app = new Vue({
 
     },
 
-    componentLogin: async function(component) {
+    componentLogin: async function (component) {
       const response = await fetch(`/dev/login/${component}`, { method: 'POST' })
       const data = await response.json()
       console.log(`${component} LOGIN: `, data)
@@ -55,7 +69,7 @@ const app = new Vue({
       this.setCredentials(component, data)
     },
 
-    setIframeURL: function(component) {
+    setIframeURL: function (component) {
       const url = new URL(this.baseURL)
       url.host = this.baseURL.host.replace(/^[^.]*\./, `${component}.`)
       url.pathname = "/listener/credentials.html"
@@ -63,13 +77,13 @@ const app = new Vue({
       this.services[component].src = url.href
     },
 
-    setCredentials: function(component, credentials) {
+    setCredentials: function (component, credentials) {
       const target = this.services[component]
       const msg = { type: 'login', component, credentials }
       document.querySelector(`#${component}`).contentWindow.postMessage(msg, target.url.origin)
     },
 
-    updateStatus: function(event) {
+    updateStatus: function (event) {
       console.log('MSG FROM IFRAME', event.data)
       const { component, loggedIn } = event.data
       if (component && this.services[component]) {
