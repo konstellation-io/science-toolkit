7f5270f1e05cdfca5f756786e69cd42d69f6b0f4
Sergio Fernandez <sergio.fernandez@intelygenz.com>
2020-05-13 13:20:43 +0200

    fix: Cert directory create error

--- scripts/create_self_signed_cert.sh
+++ scripts/create_self_signed_cert.sh
@@ -13,6 +13,10 @@ if [ -z "$NAMESPACE" ] || [ -z "$DEPLOY_NAME" ]; then
   exit 1
 fi
 
+echo "Creating self-signed CA certificates for TLS and installing them in the local trust stores"
+CA_CERTS_FOLDER=.certs/toolkit
+rm -rf ${CA_CERTS_FOLDER}/toolkit
+mkdir -p ${CA_CERTS_FOLDER}
 
 if [ -x .certs/mkcert ]; then
   echo "mkcert is installed"
@@ -22,17 +26,8 @@ else
   chmod +x .certs/mkcert
 fi
 
-echo "Creating self-signed CA certificates for TLS and installing them in the local trust stores"
-CA_CERTS_FOLDER=.certs/toolkit
-echo $CA_CERTS_FOLDER
-# This requires mkcert to be installed/available
-echo ${CA_CERTS_FOLDER}
-rm -rf ${CA_CERTS_FOLDER}
-mkdir -p ${CA_CERTS_FOLDER}
-# The CAROOT env variable is used by mkcert to determine where to read/write files
-# Reference: https://github.com/FiloSottile/mkcert
 TRUST_STORES=nss sudo .certs/mkcert --install  *.$DOMAIN
-mv _wildcard.* .certs/toolkit
+sudo mv _wildcard.* .certs/toolkit
 
 echo "Creating K8S secrets with the CA private keys"
 sudo kubectl -n $NAMESPACE create secret tls $DOMAIN-tls-secret --key=$CA_CERTS_FOLDER/_wildcard.$DOMAIN-key.pem --cert=$CA_CERTS_FOLDER/_wildcard.$DOMAIN.pem --dry-run -o yaml | kubectl apply -f -
